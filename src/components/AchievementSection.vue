eea54e55dce0a54fa5cf7c777772bfaf7f2617d9
ozo-vehe <johnnie.vehe@gmail.com>
2024-03-03 02:17:26 +0100

    adjusted the achievements section

--- src/components/AchievementSection.vue
+++ src/components/AchievementSection.vue
@@ -5,24 +5,24 @@
   <section
     class="achievements px-5 lg:px-20 md:px-16 sm:px-12 py-16 flex gap-8 flex-col items-center justify-center bg-[url('../assets/images/bg_2.png')] bg-cover bg-no-repeat bg-top text-slate-100">
     <div class="text-center">
-      <h2 class="text-[32px] font-bold capitalize">Some count that matter</h2>
+      <h2 class="lg:text-[32px] md:text-[32px] sm:text-[28px] text-[28px] font-bold capitalize">Some count that matter</h2>
       <p class="text-sm">Our achievement in the journey depicted in numbers</p>
     </div>
 
-    <div class="flex items-center gap-4 flex-wrap">
-      <p class="max-w-[250px] px-4 flex flex-col item-center justify-center text-center">
-        <span class="font-extrabold text-[24px] lg:text-[32px] md:text-[28px] sm:text-[24px]">30</span>
-        <span class="text-sm capitalize font-500">clients</span>
+    <div class="flex items-center gap-4 justify-center flex-wrap">
+      <p class="w-full lg:w-[150px] md:max-w-[250px] flex flex-col item-center justify-center text-center">
+        <span class="font-extrabold text-[20px] lg:text-[32px] md:text-[28px] sm:text-[24px]">30</span>
+        <span class="text-[12px] lg:text-sm md:text-sm capitalize font-500">clients</span>
       </p>
 
-      <p class="max-w-[250px] border-r border-l border-[#b1b1b1] p-3 flex flex-col item-center justify-center text-center">
-        <span class="font-extrabold text-[24px] lg:text-[32px] md:text-[28px] sm:text-[24px]">300+</span>
-        <span class="text-sm capitalize font-500">token business legalities</span>
+      <p class="w-full lg:w-[200px] md:max-w-[250px] lg:border-r lg:border-l md:border-r md:border-l border-[#b1b1b1] p-3 flex flex-col item-center justify-center text-center">
+        <span class="font-extrabold text-[20px] lg:text-[32px] md:text-[28px] sm:text-[24px]">300+</span>
+        <span class="text-[12px] lg:text-sm md:text-sm capitalize font-500">token business legalities</span>
       </p>
 
-      <p class="max-w-[250px] flex flex-col item-center justify-center text-center">
-        <span class="font-extrabold text-[24px] lg:text-[32px] md:text-[28px] sm:text-[24px]">8</span>
-        <span class="text-sm capitalize font-500">Years of Journey</span>
+      <p class="w-full lg:w-[150px] md:max-w-[250px] flex flex-col item-center justify-center text-center">
+        <span class="font-extrabold text-[20px] lg:text-[32px] md:text-[28px] sm:text-[24px]">8</span>
+        <span class="text-[12px] lg:text-sm md:text-sm capitalize font-500">Years of Journey</span>
       </p>
     </div>
 
