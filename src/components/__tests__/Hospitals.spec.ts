3844a0d161c7f0a62f1b710a040bf3db377c9675
ozo-vehe <johnnie.vehe@gmail.com>
2024-03-23 18:15:27 +0100

    added more unit tests

--- src/components/__tests__/Hospitals.spec.ts
+++ src/components/__tests__/Hospitals.spec.ts
@@ -37,7 +37,7 @@ describe('Markdown hospitals', () => {
   it('it should retrieve saved hospitals', async () => {
     const hospital_store = useHospitalsStore()
     await hospital_store.uploadHospital(hospital)
-    const hospitals: Array<MHospital> = await hospital_store.getSavedHospitals()
-    expect(hospitals.length).toBeGreaterThan(0)
+    const hospitals: Array<MHospital | undefined> = await hospital_store.getSavedHospitals()
+    expect(hospitals?.length).toBeGreaterThan(0)
   })
 })
